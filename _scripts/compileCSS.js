d97be24d6dc226092ad068172bdea21d65dca691
peteredwards <p.l.edwards@leeds.ac.uk>
2023-10-12 17:12:28 +0100

    added build directory for server

--- _scripts/compileCSS.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const sass = require('sass');
-
-const r = sass.compile( path.resolve( __dirname, '../_sass/style.scss' ), {style: 'compressed'} );
-
-fs.writeFileSync( path.resolve( __dirname, '../assets/css/style.css' ), r.css );
--- _scripts/minify.js
+++ _scripts/minify.js
@@ -8,5 +8,8 @@ const UglifyJS = require("uglify-js");
 const jsdir = '../_includes/javascript/';
 fs.writeFileSync( path.resolve( __dirname, '../assets/scripts/bundle.min.js' ), UglifyJS.minify({
      "utilities.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'utilities.js' ), "utf8" ),
-     "core.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'core.js' ), "utf8" )
+     "pole-of-inaccessibility.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'pole-of-inaccessibility.js' ), "utf8" ),
+     "core.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'core.js' ), "utf8" ),
+     "routing.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'routing.js' ), "utf8" ),
+     "selectercontrol.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'selectercontrol.js' ), "utf8" )
 }, { toplevel: true } ).code, "utf8" );
