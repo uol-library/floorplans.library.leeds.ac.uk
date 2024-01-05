6026171bd423e92a16e8dc586b4d300df6c8ca6b
peteredwards <p.l.edwards@leeds.ac.uk>
2024-01-05 13:54:19 +0000

    Forgot to include occupancycontrol in minification script

--- _scripts/minify.js
+++ _scripts/minify.js
@@ -11,5 +11,6 @@ fs.writeFileSync( path.resolve( __dirname, '../assets/scripts/bundle.min.js' ), 
      "pole-of-inaccessibility.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'pole-of-inaccessibility.js' ), "utf8" ),
      "core.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'core.js' ), "utf8" ),
      "routing.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'routing.js' ), "utf8" ),
-     "selectercontrol.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'selectercontrol.js' ), "utf8" )
+     "selectercontrol.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'selectercontrol.js' ), "utf8" ),
+     "occupancycontrol.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'occupancycontrol.js' ), "utf8" )
 }, { toplevel: true } ).code, "utf8" );
