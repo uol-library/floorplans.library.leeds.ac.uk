f16eaf488ba9ea0b37a94372c3ef4b19e8b063a7
Peter Edwards <p.l.edwards@leeds.ac.uk>
2025-06-12 11:14:35 +0100

    Added SVG annotations to IIIF manifests

--- _scripts/makeManifests.js
+++ _scripts/makeManifests.js
@@ -102,7 +102,7 @@ featuresXML.forEach( filename => {
         let baseURI = "https://uol-library.github.io/floorplans.library.leeds.ac.uk/assets/iiif/"+floorID;
         let XMLdata = fs.readFileSync( path.resolve( __dirname, '../assets/svg/', filename ) );
         let svgdata = parser.parse(XMLdata);
-        console.log(svgdata);
+        console.log(svgdata.svg.polygon);
         let manifest = {
             "@context": "http://iiif.io/api/presentation/3/context.json",
             "id": baseURI+"/manifest.json",
@@ -138,18 +138,18 @@ featuresXML.forEach( filename => {
                                             }
                                         ],
                                         "height": parseInt(svgdata.svg['@_viewBox'].split(',')[2])+metadata[floorID].adjust_y,
-                                        "width": parseInt(svgdata.svg['@_viewBox'].split(',')[3])+metadata[floorID].adjust_y,
+                                        "width": parseInt( svgdata.svg['@_viewBox'].split(',')[3])+metadata[floorID].adjust_y,
                                     },
-                                    "target": baseURI+"/canvas/p1"
+                                    "target": baseURI + "/canvas/p1"
                                 }
                             ]
                         }
                     ],
                     "annotations":  [
                         {
-                            "id": baseURI+"/canvas/p1/2",
+                            "id": baseURI + "/canvas/p1/2",
                             "type": "AnnotationPage",
-                            "items": getAnnotations(floorID, svgdata)
+                            "items": getAnnotations( baseURI, svgdata, metadata[floorID] )
                         }
                     ]
                 }
@@ -158,75 +158,47 @@ featuresXML.forEach( filename => {
         fs.writeFileSync( path.resolve( __dirname, '../assets/iiif/', floorID, 'manifest.json'), JSON.stringify( manifest, null, 4 ) );
     }
 });
-function getAnnotations(floorID, svgdata) {
-    return [];
+function getAnnotations( baseURI, svgdata, metadata ) {
+    let annotations = [];
+    if (svgdata.svg.polygon.length) {
+        let counter = 2;
+        svgdata.svg.polygon.forEach( a => {
+            let pointsArr = a['@_points'].split(' ');
+            let pointsArrAdjusted = [];
+            pointsArr.forEach( p => {
+                let points = p.split(',');
+                pointsArrAdjusted.push( ( parseInt( points[0] ) + metadata.adjust_x ) + ',' + ( parseInt( points[1] ) + metadata.adjust_y ) );
+            });
+            let newpoints = pointsArrAdjusted.join(' ');
+            annotations.push(
+                {
+                    "id": baseURI + "/page/p2/" + counter,
+                    "type": "AnnotationPage",
+                    "items": [
+                        {
+                            "id": baseURI + "page/p2/" + counter + "/annotation/p" + counter + "-svg",
+                            "type": "Annotation",
+                            "motivation": "tagging",
+                            "body": {
+                                "type": "TextualBody",
+                                "value": a.title,
+                                "language": "en",
+                                "format": "text/plain"
+                            },
+                            "target": {
+                                "type": "SpecificResource",
+                                "source": baseURI + "/canvas/p1",
+                                "selector": {
+                                    "type": "SvgSelector",
+                                    "value": "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><g><polygon points=\"" + newpoints + "\" fill=\"" + a['@_fill'] + "\" cursor=\"pointer\"><title>" + a.title + "</title></polygon></g></svg>"
+                                }
+                            }
+                        }
+                    ]
+                }
+            );
+        });
+        counter++;
+    }
+    return annotations;
 }
-// {
-//   "@context": "http://iiif.io/api/presentation/3/context.json",
-//   "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/manifest.json",
-//   "type": "Manifest",
-//   "label": { "en": [ metadata[floorID].title ] },
-//   "items": [
-//     {
-//       "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/canvas/p1",
-//       "type": "Canvas",
-//       "height": 3024,
-//       "width": 4032,
-//       "items": [
-//         {
-//           "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/page/p1/1",
-//           "type": "AnnotationPage",
-//           "items": [
-//             {
-//               "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/annotation/p0001-image",
-//               "type": "Annotation",
-//               "motivation": "painting",
-//               "body": {
-//                 "id": "https://iiif.io/api/image/3.0/example/reference/918ecd18c2592080851777620de9bcb5-gottingen/full/max/0/default.jpg",
-//                 "type": "Image",
-//                 "format": "image/jpeg",
-//                 "height": 3024,
-//                 "width": 4032,
-//                 "service": [
-//                   {
-//                     "id": "https://iiif.io/api/image/3.0/example/reference/918ecd18c2592080851777620de9bcb5-gottingen",
-//                     "profile": "level1",
-//                     "type": "ImageService3"
-//                   }
-//                 ]
-//               },
-//               "target": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/canvas/p1"
-//             }
-//           ]
-//         }
-//       ],
-//       "annotations": [
-//         {
-//           "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/page/p2/1",
-//           "type": "AnnotationPage",
-//           "items": [
-//             {
-//               "id": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/annotation/p0002-svg",
-//               "type": "Annotation",
-//               "motivation": "tagging",
-//               "body": {
-//                 "type": "TextualBody",
-//                 "value": "Gänseliesel-Brunnen",
-//                 "language": "de",
-//                 "format": "text/plain"
-//               },
-//               "target": {
-//                 "type": "SpecificResource",
-//                 "source": "https://iiif.io/api/cookbook/recipe/0261-non-rectangular-commenting/canvas/p1",
-//                 "selector": {
-//                   "type": "SvgSelector",
-//                   "value": "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><g><path d='M270.000000,1900.000000 L1530.000000,1900.000000 L1530.000000,1610.000000 L1315.000000,1300.000000 L1200.000000,986.000000 L904.000000,661.000000 L600.000000,986.000000 L500.000000,1300.000000 L270,1630 L270.000000,1900.000000' /></g></svg>"
-//                 }
-//               }
-//             }
-//           ]
-//         }
-//       ]
-//     }
-//   ]
-// }
