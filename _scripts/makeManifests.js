eefa045cd1ea35da1e409e8a6327c8aeb272e507
peteredwards <p.l.edwards@leeds.ac.uk>
2025-06-21 14:41:56 +0100

    Added cropped versions of images

--- _scripts/makeManifests.js
+++ _scripts/makeManifests.js
@@ -2,120 +2,38 @@ const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
 const fs = require('fs');
 const path = require('path');
 
+/* get IIIF config data */
+const configdata = fs.readFileSync( path.resolve( __dirname, '../_data/iiif.json' ) );
+const configJSON = JSON.parse(configdata);
+
+/* go through SVG files to get polygons */
 const options = {
     ignoreAttributes: false,
     attributeNamePrefix: '@_',
 };
 const parser = new XMLParser(options);
 const featuresXML = fs.readdirSync( path.resolve( __dirname, '../assets/svg' ), { encoding: 'utf8' } );
-const metadata = {
-    "brotherton-m1": {
-        "title": "Brotherton Library Main Building - level 1",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "brotherton-m2": {
-        "title": "Brotherton Library Main Building - level 2",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "brotherton-m3": {
-        "title": "Brotherton Library Main Building - level 3",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "brotherton-m4": {
-        "title": "Brotherton Library Main Building - level 4",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "brotherton-w2": {
-        "title": "Brotherton Library West Wing - level 3",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "brotherton-w3": {
-        "title": "Brotherton Library West Wing - level 3",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-10": {
-        "title": "Edward Boyle Library - level 10",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-11": {
-        "title": "Edward Boyle Library - level 11",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-12": {
-        "title": "Edward Boyle Library - level 12",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-13": {
-        "title": "Edward Boyle Library - level 13",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-8": {
-        "title": "Edward Boyle Library - level 8",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "edwardboyle-9": {
-        "title": "Edward Boyle Library - level 9",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "health-sciences": {
-        "title": "Health Sciences Library",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "laidlaw-first": {
-        "title": "Laidlaw Library - First Floor",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "laidlaw-ground": {
-        "title": "Laidlaw Library - Ground Floor",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "laidlaw-second": {
-        "title": "Laidlaw Library - Second Floor",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    },
-    "laidlaw-third": {
-        "title": "Laidlaw Library - Third Floor",
-        "adjust_x": 0,
-        "adjust_y": 0,
-    }
-};
-
 featuresXML.forEach( filename => {
     if ( filename !== '.' && filename !== '..' ) {
         let floorID = filename.substring(0,filename.length-4);
         let baseURI = "https://uol-library.github.io/floorplans.library.leeds.ac.uk/assets/iiif/"+floorID;
         let XMLdata = fs.readFileSync( path.resolve( __dirname, '../assets/svg/', filename ) );
         let svgdata = parser.parse(XMLdata);
-        let width = parseInt(svgdata.svg['@_viewBox'].split(',')[2])+metadata[floorID].adjust_x;
-        let height = parseInt(svgdata.svg['@_viewBox'].split(',')[3])+metadata[floorID].adjust_y;
+        let meta = getFloorMeta(floorID);
+        let width = parseInt(svgdata.svg['@_viewBox'].split(',')[2])+meta.adjust_x;
+        let height = parseInt(svgdata.svg['@_viewBox'].split(',')[3])+meta.adjust_y;
         let manifest = {
             "@context": "http://iiif.io/api/presentation/3/context.json",
             "id": baseURI+"/manifest.json",
             "type": "Manifest",
-            "label": { "en": [ metadata[floorID].title ] },
+            "label": { "en": [ meta.title ] },
             "items": [
                 {
                     "id": baseURI + "/canvas/p1",
                     "type": "Canvas",
                     "width": width,
                     "height": height,
-                    "label": { "en": [ metadata[floorID].title ] },
+                    "label": { "en": [ meta.title ] },
                     "items": [
                         {
                             "id": baseURI + "/canvas/p1/1",
@@ -130,7 +48,7 @@ featuresXML.forEach( filename => {
                                     "body": {
                                         "id": baseURI+"/full/max/0/default.jpg",
                                         "type": "Image",
-                                        "label": { "en": [ metadata[floorID].title ] },
+                                        "label": { "en": [ meta.title ] },
                                         "format": "image/jpeg",
                                         "service": [
                                             {
@@ -161,7 +79,7 @@ featuresXML.forEach( filename => {
                         {
                             "id": baseURI + "/canvas/p1/2",
                             "type": "AnnotationPage",
-                            "items": getAnnotations( baseURI, svgdata, metadata[floorID], floorID )
+                            "items": getAnnotations( baseURI, svgdata, meta )
                         }
                     ]
                 }
@@ -170,13 +88,13 @@ featuresXML.forEach( filename => {
         fs.writeFileSync( path.resolve( __dirname, '../assets/iiif/', floorID, 'manifest.json'), JSON.stringify( manifest, null, 4 ) );
     }
 });
-function getAnnotations( baseURI, svgdata, metadata, floorID ) {
+function getAnnotations( baseURI, svgdata, metadata ) {
     let annotations = [];
     if (svgdata.svg.polygon.length) {
         let counter = 0;
         let width = parseInt( svgdata.svg['@_viewBox'].split( ',' )[2] ) + metadata.adjust_x;
         let height = parseInt( svgdata.svg['@_viewBox'].split( ',' )[3] ) + metadata.adjust_y;
-        let svgDir = path.resolve( __dirname, '../assets/iiif/', floorID, 'svg')
+        let svgDir = path.resolve( __dirname, '../assets/iiif/', metadata.key, 'svg')
         if ( ! fs.existsSync( svgDir ) ) {
             fs.mkdirSync( svgDir );
         } else {
@@ -191,7 +109,7 @@ function getAnnotations( baseURI, svgdata, metadata, floorID ) {
                 pointsArrAdjusted.push( ( parseInt( points[0] ) + metadata.adjust_x ) + ',' + ( parseInt( points[1] ) + metadata.adjust_y ) );
             });
             let newpoints = pointsArrAdjusted.join(' ');
-            let svgBody = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0,0," + width + "," + height + "'><g><polygon style='fill:#aa0000;stroke:#888888;stroke-width:3' points='" + newpoints + "'><title>" + a.title + "</title></polygon></g></svg>";
+            let svgBody = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0,0," + width + "," + height + "'><g><polygon fill='rgba(200,50,50,0.4)' stroke='rgba(200,50,50,0.8)' points='" + newpoints + "'><title>" + a.title + "</title></polygon></g></svg>";
             fs.writeFileSync( svgDir + '/p' + counter + '.svg', svgBody );
             annotations.push(
                 {
@@ -217,4 +135,14 @@ function getAnnotations( baseURI, svgdata, metadata, floorID ) {
         });
     }
     return annotations;
+}
+function getFloorMeta(floorID) {
+    for( lib of configJSON ) {
+        for( f of lib.floors ) {
+            if ( f.key === floorID ) {
+                return f;
+            }
+        }
+    }
+    return "BASTARD!"
 }
--- iiif.md
+++ iiif.md
@@ -2,14 +2,25 @@
 layout: iiif
 permalink: /iiif/
 ---
+<h1>Original images</h1>
 {% for library in site.data.iiif %}
   <h2>{{ library.name }}</h2>
   <ul class="iiif-floors">
   {% for plan in library.floors %}
     <li>
-{% include iiif-floor.html floor=plan %}
+{% include iiif-floor.html floor=plan type='original' %}
     </li>
   {% endfor %}
   </ul>
 {% endfor %}
-</div>
+<h1>Cropped images</h1>
+{% for library in site.data.iiif %}
+  <h2>{{ library.name }}</h2>
+  <ul class="iiif-floors">
+  {% for plan in library.floors %}
+    <li>
+{% include iiif-floor.html floor=plan type='cropped' %}
+    </li>
+  {% endfor %}
+  </ul>
+{% endfor %}
